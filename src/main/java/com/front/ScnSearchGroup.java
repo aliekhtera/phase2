a16f14df404ec1e407d760682310400aac1144a7
Hanibonjakhi <97937547+Haniebonjakhi@users.noreply.github.com>
2022-08-06 10:13:09 +0430

    commit

---
+++ src/main/java/com/front/ScnSearchGroup.java
@@ -0,0 +1,38 @@
+package com.front;
+
+import com.back.messengers.Group;
+import com.back.usersPackage.User;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ScnSearchGroup implements Initializable {
+    @FXML
+    Label groupName, groupMembers, groupAdmin;
+
+    public void join() {
+        User admin = Group.getGroup().getAdmin();
+        Group.getGroup().addUser(admin, User.getLoggedInUser(), Group.getGroup());
+    }
+
+    public void leave() {
+        User admin = Group.getGroup().getAdmin();
+        Group.getGroup().removeUser(admin, User.getLoggedInUser(), Group.getGroup());
+    }
+
+    public void cancel() {
+
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        groupName.setText(Group.getGroup().getGroupName());
+        groupMembers.setText(Integer.toString(Group.getGroup().getMembers().size()) + "members");
+        groupAdmin.setText(Group.getGroup().getAdmin().getUserName());
+    }
+}
